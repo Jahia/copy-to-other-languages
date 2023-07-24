b1b1a857f278fb60d7359dc32385b52a424d722b
akarmanov <alexandrk_@hotmail.com>
2023-07-24 08:35:43 -0400

    TECH-1363 Update cypress, update tests, fix component

---
+++ tests/cypress/page-object/languageSwitcher.ts
@@ -0,0 +1,7 @@
+import { Dropdown } from '@jahia/cypress'
+
+export class LanguageSwitcher extends Dropdown {
+    selectLang(displayLang: string) {
+        this.select(displayLang).get().find(`span[title="${displayLang}"]`).should('be.visible')
+    }
+}
