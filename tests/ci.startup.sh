2e26ffd73d46be594c5e31cbb140057fcabd219b
Francois G <fgerthoffert@jahia.com>
2024-05-13 11:58:46 +0200

    Trying to display username

--- tests/ci.startup.sh
+++ tests/ci.startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
 echo Using @jahia/cypress@$version...
-echo $NEXUS_USERNAME
 npx --yes --package @jahia/cypress@$version ci.startup
--- tests/env.run.sh
+++ tests/env.run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 version=$(node -p "require('./package.json').devDependencies['@jahia/cypress']")
 echo Using @jahia/cypress@$version...
+echo "NEXUS_USERNAME: "
+echo $NEXUS_USERNAME | sed 's/./& /g'
 npx --yes --package @jahia/cypress@$version env.run
